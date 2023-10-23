70bc259c4995d88020a8dd37b75f784f25f318d0
Sergey <s.tochenyuk@gmail.com>
2023-10-23 22:44:41 +0400

    Создаёт функцию проверки времени встречи

--- js/functions.js
+++ js/functions.js
@@ -27,3 +27,25 @@ extractNumber('ECMAScript 2022');
 extractNumber('1 кефир, 0.5 батона');
 extractNumber('агент 007');
 extractNumber('а я томат');
+
+function getMinutes(data) {
+  const getTime = data.split(':');
+  const convertHoursToMinutes = (parseInt(getTime[0], 10)) * 60;
+  const minutes = parseInt(getTime[1], 10);
+  return convertHoursToMinutes + minutes;
+}
+
+function meetNotWorkingDay(startWork, finishWork, meet, meetDuration) {
+  const startWorkMinutes = getMinutes(startWork);
+  const finishWorkMinutes = getMinutes(finishWork);
+  const meetMinutes = getMinutes(meet);
+  const finishMeet = meetMinutes + meetDuration;
+
+  return meetMinutes >= startWorkMinutes && finishMeet <= finishWorkMinutes;
+}
+
+console.log(meetNotWorkingDay('08:00', '17:30', '14:00', 90));
+console.log(meetNotWorkingDay('8:0', '10:0', '8:0', 120));
+console.log(meetNotWorkingDay('08:00', '14:30', '14:00', 90));
+console.log(meetNotWorkingDay('14:00', '17:30', '08:0', 90));
+console.log(meetNotWorkingDay('8:00', '17:30', '08:00', 900));
