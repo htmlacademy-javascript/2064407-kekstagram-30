b0e3bf52ff34a90ae19d0698bff2571aac004479
Sergey <s.tochenyuk@gmail.com>
2023-11-22 14:38:24 +0400

    Делает домашку Перламутровые пуговицы

--- js/main.js
+++ js/main.js
@@ -2,12 +2,14 @@ import { renderGallery } from './gallery.js';
 import './form.js';
 import { loadPictures } from './api.js';
 import { showErrorMessage } from './util.js';
+import { initFilter } from './filters.js';
 
 
 async function bootstrap() {
   try {
     const pictures = await loadPictures();
     renderGallery(pictures);
+    initFilter(pictures);
   } catch (error) {
     showErrorMessage();
   }
